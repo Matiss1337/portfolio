b031c8b6d1cd767850e64d8479f447e3fdb498a8
Matiss <matissj1337@gmail.com>
2023-02-08 14:29:12 +0100

    content done just css left

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Box, Stack, Typography} from "@mui/material";
-import MyPhoto from "../assets/MyPhoto.png"
+
 
 const Footer = () => {
   return (
-    <Stack height={200} bgcolor="red">
-
+    <Stack height={120} alignItems="center" justifyContent="center">
+<Typography variant="h5" textAlign="center"> Designed & built by Matiss Judins 2023  </Typography>
     </Stack>
   )
 }
